5f1020d4255e962733db6f265da065ff5a22dff8
Sergey <sergey@localhost>
2021-10-09 21:24:32 +0300

    fix bugs

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,11 +1,25 @@
 fun main() {
-    var likes = 5540
+    humans(1)
+    humans(2)
+    humans(11)
+    humans(12)
+    humans(21)
+    humans(31)
+    humans(111)
+    humans(541)
+}
 
-    var human = if (((likes - 1)  % 10) == 0) {
-        "человеку"
-    } else {
+fun humans(likes: Int) {
+    var human = if ((likes - 11) % 100 == 0)
         "людям"
-    }
+    else if (likes == 11)
+        "людям"
+    else if (((likes - 1) % 10) == 0)
+        "человеку"
+    else
+        "людям"
 
     println("Понравилось $likes $human")
 }
+
+//|| ((likes - 11) % 10) == 0
